dcb0a1ff3c9b81dbabf0c220548abfb162660938
Napasv2 <120374764+Napas666@users.noreply.github.com>
2026-04-07 14:40:57 +0300

    Expose ping + reconnect events in preload

--- preload.js
+++ preload.js
@@ -1,13 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('vpnAPI', {
-  connect: (vlessKey) => ipcRenderer.invoke('vpn:connect', vlessKey),
-  disconnect: () => ipcRenderer.invoke('vpn:disconnect'),
-  getStatus: () => ipcRenderer.invoke('vpn:status'),
-  minimize: () => ipcRenderer.send('window:minimize'),
-  close: () => ipcRenderer.send('window:close'),
+  connect:    (vlessKey) => ipcRenderer.invoke('vpn:connect', vlessKey),
+  disconnect: ()         => ipcRenderer.invoke('vpn:disconnect'),
+  getStatus:  ()         => ipcRenderer.invoke('vpn:status'),
+  ping:       ()         => ipcRenderer.invoke('vpn:ping'),
+  minimize:   ()         => ipcRenderer.send('window:minimize'),
+  close:      ()         => ipcRenderer.send('window:close'),
 
-  onDownloadProgress: (cb) => ipcRenderer.on('xray:download-progress', (_e, data) => cb(data)),
-  onXrayReady: (cb) => ipcRenderer.on('xray:ready', (_e, data) => cb(data)),
-  onXrayError: (cb) => ipcRenderer.on('xray:download-error', (_e, data) => cb(data)),
+  onDownloadProgress: (cb) => ipcRenderer.on('xray:download-progress', (_e, d) => cb(d)),
+  onXrayReady:        (cb) => ipcRenderer.on('xray:ready',             (_e, d) => cb(d)),
+  onXrayError:        (cb) => ipcRenderer.on('xray:download-error',    (_e, d) => cb(d)),
+
+  // Auto-reconnect events
+  onReconnecting:    (cb) => ipcRenderer.on('vpn:reconnecting',    (_e, d) => cb(d)),
+  onReconnected:     (cb) => ipcRenderer.on('vpn:reconnected',     (_e, d) => cb(d)),
+  onReconnectFailed: (cb) => ipcRenderer.on('vpn:reconnect-failed', (_e, d) => cb(d)),
 });
